4eb28627d8b7ae7beaecbf9e9a297e8e93040f24
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-04-23 15:47:59 +0700

    Menambahkan fitur pencarian data

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -159,4 +159,26 @@ int main()
             mhs.traverse();
             break;
 
+            case '4':
+            {
+                if (mhs.listEmpty())
+                {
+                    cout << "\nList Kosong\n";
+                    break;
+                }
+
+                 Node *previous, *current;
+                 cout << "\nMasukkan no mahasiswa yang dicari : ";
+                 cin >> nim;
+
+                 if (mhs.search(nim, previous, current) == false)
+                    cout << "\nData tidak ditemukan\n";
+                 else
+                {
+                    cout << "\nData ditemukan\n";
+                    cout << "NIM Mahasiswa : " << current->noMhs << endl;
+                }
+                break;
+            }
+
            
